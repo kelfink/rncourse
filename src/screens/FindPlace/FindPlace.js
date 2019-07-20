3c80b8bfbe0d82c875b39528d56f980c93b15ffd
Kevin Fries <kelfink@yahoo.com>
2019-07-19 17:51:05 -0700

    Use redux to drive adding and listing

--- src/screens/FindPlace/FindPlace.js
+++ src/screens/FindPlace/FindPlace.js
@@ -1,13 +1,21 @@
 import React, { Component } from 'react'
 import { View, Text } from 'react-native'
+import { connect } from 'react-redux'
+import PlaceList from '../../components/PlacesList/PlacesList'
 class FindPlaceScreen extends Component {
     render() {
         return (
             <View>
                 <Text>On the FindPlaceScreen</Text>
-            </View>
+                <PlaceList places={this.props.places} />
+                </View>
         )
     }
 }
 
-export default FindPlaceScreen
+const mapStateToProps = state => {
+    return {
+        places: state.places.places
+    }
+}
+export default connect(mapStateToProps)(FindPlaceScreen)
--- src/screens/SharePlace/SharePlace.js
+++ src/screens/SharePlace/SharePlace.js
@@ -1,13 +1,26 @@
 import React, { Component } from 'react'
 import { View, Text } from 'react-native'
+import { connect } from 'react-redux'
+import InputComponent from '../../components/InputComponent/InputComponent'
+import { addPlace } from '../../store/actions/index'
+
 class SharePlaceScreen extends Component {
+    placeAddedHandler = (placeName) => {
+        this.props.onAddPlace(placeName)
+    }
     render() {
         return (
             <View>
                 <Text>On the SharePlaceScreen</Text>
+                <InputComponent onPlaceAdded={this.placeAddedHandler} />
             </View>
         )
     }
 }
 
-export default SharePlaceScreen
+const mapDispatchToProps = dispatch => {
+    return {
+        onAddPlace: (placeName) => dispatch(addPlace(placeName))
+    }
+}
+export default connect(null, mapDispatchToProps)(SharePlaceScreen)
