9da804648f8bb658b3770f96908911676c0cf09d
Kevin Fries <kelfink@yahoo.com>
2019-08-04 14:51:42 -0700

    Style for rotation on ht efly

--- src/screens/Auth/Auth.js
+++ src/screens/Auth/Auth.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { View, Button, StyleSheet, ImageBackground } from 'react-native'
+import { View, Dimensions, StyleSheet, ImageBackground } from 'react-native'
 import DefaultInput from '../../components/ui/DefaultInput/DefaultInput'
 import startMainTabs from '../MainTabs/startMainTabs'
 import HeadingText from '../../components/ui/HeadingText/HeadingText'
@@ -7,25 +7,57 @@ import MainText from '../../components/ui/MainText/MainText'
 import ButtonWithBackground from '../../components/ui/ButtonWithBackground/ButtonWithBackground'
 import image from '../../../assets/squirtle_sm.jpg'
 class AuthScreen extends Component {
+    state = {
+        viewMode: Dimensions.get("window").height > 500 ? "portrait" : "landscape"
+    }
+    constructor(props) {
+        super(props)
+        Dimensions.addEventListener("change", (dims) => {
+            console.log("dims changed", dims.window)
+            console.log("Dimension height", Dimensions.get('window').height)
+            this.setState( {
+                viewMode: Dimensions.get('window').height > 500 
+                    ? "portrait" : "landscape" 
+            })
+        })
+    }
+
     loginHandler = () => {
         startMainTabs()
     }
 
+    
     render () {
+        let headingText = null
+        if (this.state.viewMode === "portrait") {
+            headingText = <MainText><HeadingText>
+                Login Please {headingText}
+                </HeadingText></MainText>
+        }
         return (
             <ImageBackground 
             style={styles.imageBackground} 
               source={image}>
             <View style={styles.container}>
-
-              <MainText>
-                <HeadingText>Login Please</HeadingText> 
-              </MainText>
+                {headingText}
                 <ButtonWithBackground buttonType="secondary">Switch to Login</ButtonWithBackground>
                 <View style={styles.inputContainer}>
-                    <DefaultInput placeholder="Some Email Address" />
-                    <DefaultInput placeholder="Password" />
-                    <DefaultInput placeholder="Confirm Password" />
+                  <DefaultInput placeholder="Some Email Address" />
+                  <View style={
+                      this.state.viewMode === "portrait" ? styles.portraitPasswordContainer
+                      : styles.landscapePasswordContainer
+                      } >
+                    <View style={
+                        this.state.viewMode === "portrait" ? styles.portraitPasswordWrapper
+                          : styles.landscapePasswordWrapper}>
+                      <DefaultInput placeholder="Password" />
+                    </View>
+                    <View style={
+                        this.state.viewMode === "portrait" ? styles.portraitPasswordWrapper
+                          : styles.landscapePasswordWrapper}>
+                      <DefaultInput placeholder="Confirm Password" />
+                    </View>
+                  </View>
                 </View>
                 <ButtonWithBackground onPress={this.loginHandler}>Submit</ButtonWithBackground>
             </View>
@@ -50,6 +82,20 @@ const styles =  StyleSheet.create({
     input: {
         backgroundColor: "#ccc",
         borderColor: "#777"
+    },
+    landscapePasswordContainer: {
+        flexDirection:  "row",
+        justifyContent: "space-between"
+    },
+    portraitPasswordContainer: {
+        flexDirection:  "column",
+        justifyContent: "space-between"
+    },
+    landscapePasswordWrapper: {
+        width:  "45%" 
+    },
+    portraitPasswordWrapper: {
+        width:  "100%"
     }
 })
 
