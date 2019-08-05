b6849d64708735d4d92cf0bb4ffa0a160f5af347
Kevin Fries <kelfink@yahoo.com>
2019-08-05 12:08:33 -0700

    Fade in after loading

--- src/screens/FindPlace/FindPlace.js
+++ src/screens/FindPlace/FindPlace.js
@@ -1,9 +1,18 @@
 import React, { Component } from 'react'
-import { View, Text } from 'react-native'
+import { Animated, View, Text, StyleSheet, TouchableOpacity } from 'react-native'
 import { connect } from 'react-redux'
 import { deletePlace } from '../../store/actions/places'
 import PlaceList from '../../components/PlacesList/PlacesList'
+
 class FindPlaceScreen extends Component {
+    static navigatorStyle = {
+        navBarButtonColor: "brown"
+    }
+    state = {
+        placesLoaded: false,
+        removeAnimation: new Animated.Value(1),
+        loadedAnimation: new Animated.Value(0)
+    }
     constructor(props) {
         super(props);
         this.props.navigator.setOnNavigatorEvent(this.onNavigatorEvent);
@@ -17,7 +26,35 @@ class FindPlaceScreen extends Component {
                  })
             }
         }
+        // my own idea for letting us retry the animation. WHen we navigate here, mark it as unloaded.
+        this.setState({
+            placesLoaded: false
+        })
     }
+
+    placesLoadedHandler = () => {
+        console.log("Loaded Handler", this.state)
+        Animated.timing(this.state.loadedAnimation, {
+            toValue: 1,
+            duration: 2000,
+            useNativeDriver: true}).start()
+    }
+    placesSearchHandler = () => {
+        console.log("placesSearchHandler mark as loaded", this.state)
+        Animated.timing(this.state.removeAnimation, {
+            toValue: 0,
+            duration: 500,
+            useNativeDriver: true
+           }).start(() => {
+            this.setState({
+                placesLoaded: true
+            })
+            this.placesLoadedHandler();
+       })
+
+        //this.setState({placesLoaded: true})
+    }
+
     itemSelectedHandler = key => {
         const selPlace = this.props.places.find(place => {
             return place.key === key;
@@ -32,15 +69,61 @@ class FindPlaceScreen extends Component {
         })
     }
     render() {
+        let content = this.state.placesLoaded
+                ?  ( 
+                    <Animated.View style={{
+                        opacity: this.state.loadedAnimation
+                          }}>
+                       <PlaceList places={this.props.places} itemSelected={this.itemSelectedHandler}/>
+                    </Animated.View>)
+                : (<Animated.View style={{
+                      opacity: this.state.removeAnimation,
+                      transform: [
+                          {
+                              scale: this.state.removeAnimation.interpolate({
+                                  inputRange: [0, 1],
+                                  outputRange: [2, 1]
+                              })
+                          }
+                      ]
+                }}>
+                    <TouchableOpacity onPress={this.placesSearchHandler}>
+                    <View style={styles.searchButton}>
+                        <Text style={styles.searchButtonText}>
+                                Loading maybe
+                        </Text>
+                    </View>
+                   </TouchableOpacity>
+                 </Animated.View>)
         return (
-            <View>
-                <Text>On the Find Place Screen</Text>
-                <PlaceList places={this.props.places} itemSelected={this.itemSelectedHandler}/>
-                </View>
+            <View style={this.state.placesLoaded ? null : styles.buttonContainer}>
+                  {content}
+            </View>
         )
     }
 }
 
+const styles = StyleSheet.create({
+    buttonContainer: {
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center"
+    },
+    listContainer: {
+
+    },
+    searchButton: {
+        borderColor: "orange",
+        borderWidth: 1,
+        borderRadius: 50,
+        padding: 20
+    },
+    searchButtonText: {
+        color: "orange",
+        fontWeight: "bold",
+        fontSize: 26
+    }
+})
 const mapStateToProps = state => {
     return {
         places: state.places.places
